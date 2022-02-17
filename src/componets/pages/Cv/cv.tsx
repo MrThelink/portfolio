f78d9fee2e18072bc6a26cd163f9767ba9844108
MrThelink <deni.israilov@Ohjelmointi-13-MacBook-Pro.local>
2022-02-17 15:24:29 +0200

    I got corrected the main and cv

--- src/componets/pages/Cv/cv.tsx
+++ src/componets/pages/Cv/cv.tsx
@@ -10,29 +10,25 @@ const Cv: React.FC<CvProps> = () => {
     <div className={styles.container}>
         <div className={styles.about_text}>
             <div className={styles.section_title}>
-              <h2>About Me</h2>
+              <h2>Education</h2>
             </div>
             <div className={styles.p_text}>
               <p>Currently studying Bachelor of Degree in Information and Communication Technology, Varia 12.08.2020 - 03.06.2023</p>
             </div>
             <div className={styles.padd}>
                 <h3>SKILLS</h3>
-                <div className={styles.skill}>
+                <p>Agile software development Participating on small scrum teams
+                    Writing user stories Working with clients to develop product backlogs</p>
+              <div className={styles.skill}>
                 <div className={styles.skills_item}>html</div>
                 <div className={styles.skills_item}>css</div>
-                <div className={styles.skills_item}>javasScript</div>
-                <div className={styles.skills_item}>typesScript</div>
                 <div className={styles.skills_item}>c#</div>
                 <div className={styles.skills_item}>scss</div>
+                <div className={styles.skills_item}>javasScript</div>
+                <div className={styles.skills_item}>typesScript</div>
                 <div className={styles.skills_item}>react</div>
                 <div className={styles.skills_item}>unity</div>
                 <div className={styles.skills_item}>wordpress</div>
-            </div>
-
-            <div className={styles.about_tabs}>
-                <button className={styles.tab_item}>
-                  Download CV
-                </button>
             </div>
             </div>
         </div>
