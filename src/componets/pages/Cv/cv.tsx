d4e7ab2123e5436fdbd9a38a3d457bb4804ad25b
MrThelink <deni.israilov@Ohjelmointi-13-MacBook-Pro.local>
2022-03-03 14:16:39 +0200

    Added a couple of changes to the portfolio and now I'm doing cv

--- src/componets/pages/Cv/cv.tsx
+++ src/componets/pages/Cv/cv.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaCss3, FaHtml5, FaReact } from 'react-icons/fa'
 import styles from "./cv.module.scss"
 
 type CvProps = {
@@ -28,8 +29,37 @@ const Cv: React.FC<CvProps> = () => {
 
             <div className={styles.padd}>
                 <h2>SKILLS</h2>
-                <p>Agile software development Participating on small scrum teams  
-                    Writing user stories Working with clients to develop product backlogs</p>
+                <p>Tools and programming languages that I use for development and making projects</p>
+                <div className={styles.skill_resul}>
+                    <div className={styles.skill_box}>
+                      <a href="">
+                          <FaHtml5/>
+                      </a>
+                      <h3>HTML 5</h3>
+                      <h4>I use this laungitsh when I am doing website</h4>
+                    </div>
+
+                    <div className={styles.skill_box}>
+                    <div>
+                      <a href="" id={styles.css}>
+                          <FaCss3/>
+                      </a>
+                      <h3>CSS 3</h3>
+                      <h4>I use this laungitsh to dissan my website</h4>
+                    </div>
+                  </div>
+
+                  <div className={styles.skill_box}>
+                    <div>
+                      <a href="" id={styles.css}>
+                          <FaReact/>
+                      </a>
+                      <h3>React</h3>
+                      <h4>I use this laungitsh to dissan my website</h4>
+                    </div>
+                  </div>
+
+                </div>
             </div>
 
             <div className={styles.lan_ski}>
