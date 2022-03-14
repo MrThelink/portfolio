a0d7d4f4d8e1371841d66368bfe6eb4e72d9d5ba
MrThelink <deni.israilov@Ohjelmointi-13-MacBook-Pro.local>
2022-03-14 13:22:00 +0200

    add new(projects in project)

--- src/componets/pages/Proects/proects.tsx
+++ src/componets/pages/Proects/proects.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaCss3, FaHtml5, FaReact, FaUnity } from "react-icons/fa"
+import { FaCss3, FaHtml5, FaJava, FaReact, FaUnity } from "react-icons/fa"
 import { DiJavascript, DiSass } from "react-icons/di"
 import { SiCsharp, SiTypescript} from "react-icons/si";
 import styles from "./proects.module.scss"
@@ -26,7 +26,7 @@ const Proects: React.FC<ProectsProps> = () => {
                 The game is maze you need found the way out.
                 </p>
           <p className={styles.unity}>
-            <FaUnity size={20}/>Unity <SiCsharp size={15}/>C#
+            <FaUnity size={20} color="#4c5d70"/>Unity <SiCsharp size={15} color="#68217A"/>C#
           </p>
           <div className={styles.buttons}>
             <a href="https://play.unity.com/mg/other/find-the-way-out-1" rel="noreferrer" target='_blank'>
@@ -45,7 +45,7 @@ const Proects: React.FC<ProectsProps> = () => {
             This is my school portfolio project.
           </p>
           <p className={styles.skills_}>
-             <SiTypescript size={15}/>TS <FaReact size={15}/>React  <DiSass/>Scss
+             <SiTypescript size={15} color="#007ACC"/>TS <FaReact size={15} color="#61DAFB"/>React  <DiSass color='#CB6699'/>Scss
           </p>
           <div className={styles.buttons}>
             <a href='https://github.com/MrThelink/portfolio' rel="noreferrer" target='_blank'>
@@ -63,10 +63,47 @@ const Proects: React.FC<ProectsProps> = () => {
             This is my own calculator project.
           </p>
           <p className={styles.skills_}>
-            <FaHtml5 size={15}/>Html  <FaCss3 size={15}/>Css <DiJavascript size={15}/>JavaScript
+            <FaHtml5 size={15} color="#F16529"/>Html  <FaCss3 size={15} color="#33A9DC"/>Css <DiJavascript size={15} color="#F0DB4F"/>JavaScript
           </p>
           <div className={styles.buttons} id={styles.butstyl}>
             <a href='https://github.com/MrThelink/Calculator' rel="noreferrer" target='_blank'>
+              <p>
+                Learn More
+              </p>
+            </a>
+          </div>
+        </div>
+        <div className={styles.box}>
+          <h2>
+            Team school Java game project
+          </h2>
+          <p className={styles.box_topText}>
+            This is rpg Java game. I made this project with classmates (GAME NAME: Magic world).
+          </p>
+          <p className={styles.box_top} >
+            <FaJava size={15} color="red"/>Java
+          </p>
+          <div className={styles.buttons} id={styles.butstyl}>
+            <a href='https://github.com/Jebu04/Peli-projekti/tree/main' rel="noreferrer" target='_blank'>
+              <p>
+                Learn More
+              </p>
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <h2>
+            School classmates game project.
+          </h2>
+          <p className={styles.box_topText}>
+            This in classmates game project (GAME NAME: Partin maatila)
+          </p>
+          <p className={styles.box_top}>
+            <FaUnity size={20} color="#4c5d70"/>Unity  <SiCsharp size={15} color="#68217A"/>C#
+          </p>
+          <div className={styles.buttons} id={styles.butstyl}>  
+            <a href='https://github.com/zeta404-ops/varia-agile-20C-ryhma-3' rel="noreferrer" target='_blank'>
               <p>
                 Learn More
               </p>
