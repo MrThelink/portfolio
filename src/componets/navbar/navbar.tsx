06e28f89058987befcfb5b7da071573fccc9ac61
MrThelink <deni.israilov@Ohjelmointi-13-MacBook-Pro.local>
2022-03-30 09:32:42 +0300

    fix(navbar: line 4)

--- src/componets/navbar/navbar.tsx
+++ src/componets/navbar/navbar.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from "../navbar/navbar.module.scss"
-import { GiHamburgerMenu } from "react-icons/gi"
 import { Logo , StyleLogo } from "../navbar/logo"
 import Hamburger from 'hamburger-react'
 
